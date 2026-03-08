632afffbbff80ed30fa5550fe997e1aab5763fa2
Nirmalspatel <Nirmalspatel3003@gmail.com>
2026-03-08 21:07:20 +0530

    update middleware in utills

---
+++ src/db/index.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+
+
+const connectDB = async(url)=>{
+  try {
+    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+    console.log("MONGO DB CONNECTED: ", connectionInstance.connection.host);
+  } catch (error) {
+    console.log("MONGO DB CONNECTION ERROR: ", error);
+    process.exit(1);
+
+  }
+}
+
+export default connectDB;
---
+++ src/index.js
@@ -0,0 +1,26 @@
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+import { app } from "../src/app.js";
+
+
+dotenv.config({
+  path:'/env'
+});
+
+
+
+connectDB().then(()=>{
+  app.listen(process.env.PORT||8000,()=>{
+    console.log(`SERVER STARTED ON PORT ${process.env.PORT}`);
+  })
+  
+}).catch((error)=>{
+  console.log("DB CONNECTION ERROR: ", error);
+} );
+
+
+
+
+
+
+
